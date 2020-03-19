7148a4c8b3bb76a8b5c7f607ba5159d33e5ab83c
MasterfulDingo <Lachlan.Row@gmail.com>
2020-03-20 10:22:33 +1300

    first commit

---
+++ script.py
@@ -0,0 +1,36 @@
+import sqlite3
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [ #context for making context
+    {
+        'author' : 'lachlan row',
+        'title' : 'first post',
+        'content' : 'first post content',
+        'date_posted' : 'february 10 2020'
+    },
+    {
+        'author' : 'john doe',
+        'title' : 'second post',
+        'concent' : 'second post concent',
+        'date posted' : 'april 1 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts=posts)
+
+@app.route('/all_spells')
+def all_spells():
+
+    conn = sqlite3.connect('spells.db')
+    cur = conn.cursor()
+    cur.execute('SELECT name FROM spell')
+    results = cur.fetchall()
+
+    return render_template('all_spells.html', spells=results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
