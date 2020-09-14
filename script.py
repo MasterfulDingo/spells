b1a0e87e0f50cffc27b2c89d50a654d9836205f5
MasterfulDingo <Lachlan.Row@gmail.com>
2020-09-14 17:08:46 +1200

    complete functionality for filtering by class

--- script.py
+++ script.py
@@ -111,7 +111,6 @@ def db_func(sql,single):
 
 def filtermfunc(filterraw):
     sql = [] #setting up some empty lists and variables for use in formatting the returned data.
-    
     schools = []
     schoolstr = "spell.school IN ({})"
 
@@ -125,7 +124,7 @@ def filtermfunc(filterraw):
     ritualstr = "spell.ritual IN ({})"
 
     caster = []
-    casterstr = "spell.id IN (SELECT sid FROM spellcaster WHERE cid == {})"
+    casterstr = "spell.id IN (SELECT sid FROM spellcaster WHERE cid == {})" #nested SQL query, gathering all of the spell ids from the joining table "spellcaster" where the caster id matches that of the id given, which is gathered later in the function.
 
     for term in filterraw: #iterates through the returned terms from the form in the "search" page
         term = term.split("_") #splits each term by the underscore, as they were connected when being sent from the webpage. this gives us the first term, the column name in the database in the table "spells", and the value that we want to search by, which will need formatting in the following functions.
@@ -134,9 +133,9 @@ def filtermfunc(filterraw):
             schools.append(str(term[1])) #turns the index into a string and puts it in the list
         elif term[0] == "level": 
             levels.append(str(term[1])) 
-        elif term[0] == "caster":
-            term [1] = entfields[2].index(term[1])
-            caster.append(casterstr.format(str(term[1])))
+        elif term[0] == "casters":
+            term[1] = entfields[4].index(term[1]) #this acts much the same as the function used at the start of this if statement for schools, gathering the index of the matching term in the fifth list in the main list 'entfields' and later comparing it to the caster id in the joining table
+            caster.append(casterstr.format(str(term[1]))) #for each spellcaster, a different list of spells must be returned, so the sql string used before is combined together with OR parameters. The database will only return the information for each spell once, so if multiple classes can cast the same spell there is no problem
         elif term[0] == "concentration":
             term[1] = (entfields[2].index(term[1]))-1 #with concentration and ritual, in the database they are represented as 0 and 1, so i must subtract one from their indexes in the list "entfields"
             concentration.append(str(term[1]))
@@ -152,9 +151,8 @@ def filtermfunc(filterraw):
         levelstr = levelstr.format(", ".join(levels))
         sql.append(levelstr)
     if caster:
-        casterstr = " OR ".join(caster)
+        casterstr = " OR ".join(caster) #joining the nested SQL queries with OR statements so it returns the spells that are available to any of the selected classes
         sql.append(casterstr)
-        print(casterstr)
     if concentration:
         concentrationstr = concentrationstr.format(", ".join(concentration))
         sql.append(concentrationstr)
@@ -163,7 +161,6 @@ def filtermfunc(filterraw):
         sql.append(ritualstr)
     sqlstring = str(" AND ".join(sql)) #joins the completed strings together via SQL conventions
     sqlstring = " WHERE {}".format(sqlstring) #throws a "WHERE" on the front to make the statement a parameter! this SQL parameter is now complete!
-    print(sqlstring)
     return sqlstring
 
 if __name__ == "__main__":
